51aa0dafc5413dd2e0e677318feee969986861ad
hilderarce <hilderarce566@gmail.com>
2025-11-28 03:37:35 -0500

    xd

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +11,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('hilder-front');
+export class App implements OnInit{
+
+  constructor(private HTTP: HttpClient){}
+
+  async ngOnInit(): Promise<void> {
+    try {
+      const response = await firstValueFrom(
+        this.HTTP.get(`${environment.apiUrl}/users`)
+      );
+
+      console.log('Datos recibidos:', response);
+
+    } catch (error) {
+      console.error('Error al llamar la API:', error);
+    }
+  }
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://api.hilderarce.com/v1'
+};
